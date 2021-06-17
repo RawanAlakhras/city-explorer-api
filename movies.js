095af313c6804488fa59b5b53691ff58c872313c
RawanAlakhras <ralakhras36@gmail.com>
2021-06-17 14:12:15 +0300

    complete lab-10

---
+++ movies.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+module.exports = movies;
+let prevMovArray = {};
+async function movies(req, res) {
+    //res.send('hello')
+    let searchQuery = req.query.searchQuery;
+    let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+
+
+    if (prevMovArray[searchQuery] != undefined) {
+        
+        console.log('from prevMovArray');
+        res.send(prevMovArray[searchQuery]);
+    }
+    else {
+        try {
+            const movieResult = await axios.get(movieURL);
+            //res.send(movieResult.data.results[0]);
+            //console.log(movieResult.data.results[0]);
+            console.log(movieResult.data);
+            let movieArr = movieResult.data.results.map(item => {
+                return (new Corresponding(item));
+            });
+            //console.log('rawan'+movieArr[0].data);
+           
+            //console.log(movieArr.vote_count)
+            prevMovArray[searchQuery] = movieArr;
+            console.log('from API');
+            res.send(movieArr);
+        }
+        catch (err) {
+            res.status(500).send('Sorry there is error: ' + err)
+        }
+    }
+}
+
+
+class Corresponding {
+    constructor(data) {
+        this.title = data.title;
+        this.overview = data.overview;
+        this.vote_average = data.vote_average;
+        this.vote_count = data.vote_count;
+        this.imgURL = 'https://image.tmdb.org/t/p/w500' + data.poster_path;
+        this.popularity = data.popularity;
+        this.release_date = data.release_date;
+    }
+}
+
